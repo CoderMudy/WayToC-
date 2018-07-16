2b4b2dca6f2168047b9d4badfa36cac1a1e62a27
coderMudy <2808050775@qq.com>
2018-07-16 15:40:03 +0800

    继承

---
+++ day01/Inherit/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Inherit
+{
+    class Shape
+    {
+        protected int width;
+        protected int height;
+        public void setWidth(int w)
+        {
+            width = w;
+        }
+
+        public void setHeight(int h)
+        {
+            height = h;
+        }
+    }
+
+    /**
+     * c#不支持多继承。但是可以通过接口来实现多重继承
+     */
+
+    public interface PaintCost
+    {
+        Console.WriteLine("接口中的代码");
+        int getCost(int area);
+    }
+
+    class Rectangle : Shape,PaintCost
+    {
+        public int getArea()
+        {
+            return width * height;
+        }
+
+        public int getCost(int area)
+        {
+            return area * 80;
+        }
+    }
+
+    class Tester
+    {
+     static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+            r.setHeight(5);
+            r.setWidth(7);
+            int area = r.getArea();
+            Console.WriteLine(area);
+            Console.WriteLine("总成本:{0}", r.getCost(area));
+            Console.ReadKey();
+        }
+    }
+}
