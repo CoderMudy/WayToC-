cd13bbbc4c919bb52df703c630bfb31e4dfdb5b1
coderMudy <2808050775@qq.com>
2018-07-17 11:03:37 +0800

    Jumony的导入与简单使用

---
+++ day01/demo01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ivony.Html;
+using Ivony.Html.Parser;
+
+namespace demo01
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var parser = new JumonyParser();
+
+            var document = parser.LoadDocument("http://www.cnblogs.com/");
+ 
+            var elements = document.Find("#post_list > .post_item");
+            foreach (var e in elements)
+            {
+                Console.WriteLine("title: {0}", e.FindFirst("h3 > a").InnerText());
+                Console.WriteLine("link: {0}", e.FindFirst("h3 > a").Attribute("href").Value());
+                Console.WriteLine("votes: {0}", e.FindFirst(".diggnum").InnerText());
+                Console.WriteLine("author: {0}", e.FindFirst(".post_item_foot a.lightblue").InnerText());
+                Console.WriteLine(new string('-', 30));
+                Console.ReadKey();
+            }　 
+        }
+    }
+
+
+}
